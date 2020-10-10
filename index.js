6cce6d05cce84a6b9f808c5ec2523f57473793a9
Michael Hamilton <michaelhamilton626@gmail.com>
2020-10-10 00:44:52 -0400

    adds persistent storage

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
-const express = require('express');
-const drivelist = require('drivelist');
-const cors = require('cors');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const drivelist = require('drivelist');
+const express = require('express');
+const fs = require('fs');
+const storage = require('node-persist');
 
 const app = express();
 
-let VIDEO_PATH = '/';
+(async () => {
+  await storage.init({dir: 'APPDATA'});
 
-app.use(express.static(`${__dirname}/dist`));
-app.use(cors());
-app.use(bodyParser.json());
+  let VIDEO_PATH = await storage.getItem('VIDEO_PATH') || '/';
 
-const createDynStatic = (path) => {
-  let st = express.static(path)
-  let dyn = (req, res, next) => st(req, res, next);
-  dyn.setPath = (newPath) => st = express.static(newPath);
-  return dyn;
-}
+  app.use(express.static(`${__dirname}/dist`));
+  app.use(cors());
+  app.use(bodyParser.json());
 
-const dyn = createDynStatic(VIDEO_PATH);
-app.use(dyn);
-
-app.get('/', async (req, res, next) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
-});
-
-app.get('/drivelist', async (req, res, next) => {
-  res.send(await drivelist.list());
-});
-
-app.get('/videolist', async (req, res, next) => {
-  try {
-    let files = fs.readdirSync(VIDEO_PATH);
-    files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
-    res.send(files.map(file => encodeURI(file)));
+  const createDynStatic = (path) => {
+    let st = express.static(path)
+    let dyn = (req, res, next) => st(req, res, next);
+    dyn.setPath = (newPath) => st = express.static(newPath);
+    return dyn;
   }
-  catch(err) {
-    res.send([]);
-  }
-});
 
-app.get('/videopath', (req, res, next) => {
-  res.send(VIDEO_PATH);
-});
+  const dyn = createDynStatic(VIDEO_PATH);
+  app.use(dyn);
 
-app.post('/videopath', (req, res, next) => {
-  VIDEO_PATH = req.body.videoPath;
-  dyn.setPath(VIDEO_PATH);
-  res.send('ok');
-});
+  app.get('/', async (req, res, next) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
+  });
 
-app.get('*', (req, res, next) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
-});
+  app.get('/drivelist', async (req, res, next) => {
+    res.send(await drivelist.list());
+  });
 
-app.listen(3000);
+  app.get('/videolist', async (req, res, next) => {
+    try {
+      let files = fs.readdirSync(VIDEO_PATH);
+      files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+      res.send(files.map(file => encodeURI(file)));
+    } catch (err) {
+      res.send([]);
+    }
+  });
+
+  app.get('/videopath', (req, res, next) => {
+    res.send(VIDEO_PATH);
+  });
+
+  app.post('/videopath', async (req, res, next) => {
+    VIDEO_PATH = req.body.videoPath;
+    dyn.setPath(VIDEO_PATH);
+    await storage.setItem('VIDEO_PATH', VIDEO_PATH);
+    res.send('ok');
+  });
+
+  app.get('*', (req, res, next) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
+  });
+
+  app.listen(3000);
+})();
