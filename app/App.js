5e5acbd2ff72263821a0b9441489667bab6bb1e9
Michael Hamilton <michaelhamilton626@gmail.com>
2020-10-18 12:52:59 -0400

    adds very basic pin code lockout

--- app/App.js
+++ app/App.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
+  Redirect,
   Route,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
 import Config from './screens/Config';
+import Login from './screens/Login';
 import Videos from './screens/Videos';
 import './styles.scss';
 
-const App = props => (
-  <Router>
-    <div className={'app-container'}>
-      <div className={'header'}>
-        <h1>vidserve</h1>
-        <div className={'nav'}>
-          <NavLink exact to={'/'}>videos</NavLink>
-          <NavLink exact to={'/config'}>config</NavLink>
-        </div>
+const App = props => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <div className={'app-container'}>
+        {
+          isLoggedIn ?
+            <div className={'header'}>
+              <h1>vidserve</h1>
+              <div className={'nav'}>
+                <NavLink exact to={'/'}>videos</NavLink>
+                <NavLink exact to={'/config'}>config</NavLink>
+              </div>
+            </div> :
+            null
+        }
+        <Switch>
+          <Route path={'/config'}>
+            {
+              isLoggedIn ?
+                <Config /> :
+                <Login
+                  login={() => setLoggedIn(true)}
+                  isLoggedIn={isLoggedIn}
+                />
+            }
+          </Route>
+          <Route path={'/'}>
+            {
+              isLoggedIn ?
+                <Videos /> :
+                <Login
+                  login={() => setLoggedIn(true)}
+                  isLoggedIn={isLoggedIn}
+                />
+            }
+          </Route>
+        </Switch>
       </div>
-      <Switch>
-        <Route path={'/config'}>
-          <Config />
-        </Route>
-        <Route path={'/'}>
-          <Videos />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+}
 
 export default App;
---
+++ app/screens/Login/index.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './styles.scss';
+
+const PIN_CODE = '1234';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pin: null,
+    };
+  }
+
+  handlePasswordChange(e) {
+    this.setState({pin: e.target.value});
+  }
+
+  async login(e) {
+    e.preventDefault();
+    if (this.state.pin === PIN_CODE) {
+      this.props.login();
+    }
+    else {
+      alert('Incorrect pin.');
+    }
+  }
+
+  render() {
+    return (
+      <div className={'login-container'}>
+        {
+          !this.props.isLoggedIn ? (
+            <div className={'login-form-wrapper'}>
+              <h1 className={'login-heading'}>vidserve</h1>
+              <form
+                className={'form'}
+                onSubmit={(e) => this.login(e)}
+              >
+                <input
+                  onChange={(e) => this.handlePasswordChange(e)}
+                  placeholder={'pin'}
+                  type={'password'}
+                  inputmode={'numeric'}
+                  maxlength="4"
+                  pattern="[0-9]{4}"
+                  value={this.state.pin}
+                />
+                <button type={'submit'}>login</button>
+              </form>
+            </div>
+          ) :
+            <div className={'login-form-wrapper'}>
+              <h1 className={'login-heading'}>vidserve</h1>
+              <h2 className={'login-subheading'}>logged in</h2>
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default Login;
